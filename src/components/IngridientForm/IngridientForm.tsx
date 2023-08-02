9431d2bc2a0c7e264e479275b40fd130ceb42c46
Farid <faridqasimov8@gmail.com>
2023-08-03 00:46:36 +0400

    validation

--- src/components/IngridientForm/IngridientForm.tsx
+++ src/components/IngridientForm/IngridientForm.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react'
 import classes from './IngridientForm.module.css'
-import {useSelector} from 'react-redux'
-import {Items,Ing} from '../../types/type'
 import {useHook} from '../hooks/customHook'
-import { useAppSelector,useAppDispatch } from '../store/hooks'
+import { useAppSelector } from '../store/hooks'
 
 
-
+type V={
+    title:string,
+    amount:string,
+    [key:string]:string
+}
 
 const IngridientForm =()=>{
     const items = useAppSelector(state=>state.mainSt.items)
-   const dispatch =useAppDispatch()
-    console.log(items,'itititititi')
+    const [foc,setFoc] = useState(false)
+    const [amt,setAmt] = useState(false)
+
 const [val,setVal] =useState({
     title:'',
     amount:''
@@ -25,6 +28,16 @@ const {sendRequest} = useHook('',itmsInd,val,ind,true)
             return {...prevSt,[name]:value}
         })
     }
+
+    const focTitle=()=>{
+     setFoc(true)
+    }
+    const focAmount =()=>{
+        setAmt(true)
+    }
+
+    const validation =Object.values(val).some(item=>item === '')
+ console.log(validation,'bblblblbbl')
     let URL:any
     let ITM:any
     
@@ -34,22 +47,23 @@ const {sendRequest} = useHook('',itmsInd,val,ind,true)
     ITM = ind >=0 ? {...items[ind],amount:parseInt(items[ind].amount) + parseInt(val.amount)} : val
     URL =ind<0 ? `https://auth-with-hooks-default-rtdb.firebaseio.com/form/.json` :
     `https://auth-with-hooks-default-rtdb.firebaseio.com/form/${items[ind].id}.json`
-    console.log(ITM,ind,'ind')
-    sendRequest(URL,ind>= 0 ? 'PUT': 'POST',ITM)
     
+        sendRequest(URL,ind>= 0 ? 'PUT': 'POST',ITM)
     }
 
     return(
         <form className={classes.form} onSubmit={submitHandler}>
-           <div className={classes.inp}>
+           <div className={`${classes.inp} ${!val.title && foc && classes.title}`}>
             <label htmlFor='name'>Name</label>
-            <input type='text' id='name' name='title' placeholder='Please enter a value' onChange={changeHandler}/>
+            <input  type='text' id='name' name='title' placeholder='Please enter a value' onChange={changeHandler} onBlur={focTitle}/>
+            <p style={{color:'rgb(201, 69, 100)',margin:0}}>{!val.title && foc && 'Please enter the title'}</p>
            </div>
-           <div className={classes.inp}>
+           <div className={`${classes.inp} ${!val.amount && amt && classes.amount}`}>
             <label htmlFor='amount'>Amount</label>
-            <input type='number' id='amount' name='amount' minLength={1} maxLength={5} onChange={changeHandler}/>
+            <input  type='number' id='amount' name='amount' minLength={1} maxLength={5} onChange={changeHandler} onBlur={focAmount}/>
+            <p style={{color:'rgb(201, 69, 100)',margin:0}}>{!val.amount && amt && 'Please enter amount'}</p>
            </div>
-           <button type='submit'>Add Ingredient</button>
+           <button className={`${classes.btn} ${validation && classes.valid}`} type='submit'>Add Ingredient</button>
         </form>
     )
 }
